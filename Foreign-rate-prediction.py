67ab403bc8c2c29c659bff4acbe9a73f9c4c2894
saksham <sakshambassi@gmail.com>
2019-05-17 10:57:11 +0530

    changes in py

--- Foreign-rate-prediction.py
+++ Foreign-rate-prediction.py
@@ -13,23 +13,26 @@ def entropy1(labels, base=None):
   return entropy(counts, base=base)
 
 df = pd.read_csv("/home/saksham/TIFR-CAM/Finance/Foreign Exchange/USA-AUS-exchange-rates.csv")
+# ND in USA-AUS, '.' in csv downloaded from FRED
 df = df[df.iloc[:, 1] != 'ND']
-df = df.iloc[:-2, 1].astype(float)
+# df = df.convert_objects(convert_numeric=True)
+df = df.iloc[:, 1].astype('float64')
 df = df.values
+df = 1/df
+print(df)
 
-
-train = df[0: int(math.floor(0.75 * len(df)))]
-valid = df[int(math.floor(0.75 * len(df))):]
+train = df[0: int(math.floor(0.8 * len(df)))]
+valid = df[int(math.floor(0.8 * len(df))):]
 
 print(df.shape)
 
 
 print(train.shape, valid.shape)
 
-
+num_days = 7
 x_train, y_train = [], []
-for i in range(7,len(train)):
-	x_train.append(train[i-7:i])
+for i in range(num_days,len(train)):
+	x_train.append(train[i-num_days:i])
 	y_train.append(train[i])
 x_train, y_train = np.array(x_train), np.array(y_train)
 
@@ -41,11 +44,11 @@ y_train.shape
 
 # In[29]:
 
-inputs = df[len(train) - 7:]
+inputs = df[len(train) - num_days:]
 x_test = []
 
-for i in range(7,inputs.shape[0]):
-	x_test.append(inputs[i-7:i])
+for i in range(num_days,inputs.shape[0]):
+	x_test.append(inputs[i-num_days:i])
 x_test = np.array(x_test)
 print(x_test.shape)
 
@@ -97,7 +100,7 @@ def CNN():
 	model.add(keras.layers.Dense(1))
 	model.summary()
 	model.compile(loss=keras.losses.mse, optimizer=keras.optimizers.Adam())
-	model.fit(x_train, y_train, epochs=50, batch_size=8, verbose=1, validation_data=(x_test, valid) )
+	model.fit(x_train, y_train, epochs=200, batch_size=8, verbose=1, validation_data=(x_test, valid) )
 	return model
 
 model = CNN()
@@ -106,9 +109,11 @@ closing_price = model.predict(x_test)
 rms=np.sqrt(np.mean(np.power((valid-closing_price),2)))
 print("RMS:",rms)
 
+least_abs_mean = np.mean(valid-closing_price)
+print("Least abs mean:", least_abs_mean)
 
 import seaborn as sns
-sns.set(font_scale = 1.8)
+sns.set(font_scale = 2.2)
 
 
 # In[49]:
@@ -116,11 +121,14 @@ sns.set(font_scale = 1.8)
 train = y_train
 closing_price_x = np.arange(len(y_train), len(y_train) + len(closing_price), 1)
 valid_x = np.arange(len(y_train), len(y_train) + len(valid), 1)
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(16,8))
+plt.ylabel("Rate")
+plt.xlabel("Days")
 plt.plot(train, label = 'Train Data')
 plt.plot(valid_x, valid, label = 'Test Data')
 plt.plot(closing_price_x, closing_price, label = 'Prediction')
 plt.legend()
+plt.savefig("Pred-on-test-with-train.pdf")
 plt.show()
 
 
@@ -128,7 +136,10 @@ plt.show()
 
 # y_pred = model.predict(x_train)
 plt.figure(figsize=(20,10))
-plt.plot(closing_price.ravel(), label = 'Predicted Price')
-plt.plot(valid.ravel(), label = 'Validation Set Price')
-plt.legend()
+plt.ylabel("Rate")
+plt.xlabel("Days")
+plt.plot(closing_price.ravel(), label = 'Predicted Rate')
+plt.plot(valid.ravel(), label = 'Actual Rate')
+plt.legend()	
+plt.savefig("Pred-on-test.pdf")
 plt.show()
